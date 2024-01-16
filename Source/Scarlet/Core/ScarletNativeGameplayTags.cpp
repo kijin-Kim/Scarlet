b39160ca11365661d5716b2f998fd62103961d36
kijin-Kim <kijin.kim0@gmail.com>
2024-01-16 12:52:08 +0900

    feat: 기본 움직임 로직 및 Animation Blueprint 로코모션 로직 추가

--- Source/Scarlet/Core/ScarletNativeGameplayTags.cpp
+++
@@ -1,8 +0,0 @@
-#include "ScarletNativeGameplayTags.h"
-
-namespace ScarletNativeGameplayTags
-{
-	UE_DEFINE_GAMEPLAY_TAG(Input_Move, "Input.Move");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Jump, "Input.Jump");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Look, "Input.Look");
-}
